09726b94cc324b6e17a3b893b932c1d7a8ca2bd5
Shuichi Tsukamoto <shtsukam@microsoft.com>
2017-06-07 11:33:27 +0900

    change  bot.src from constant to enviroment variable.

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -3,13 +3,15 @@ package controllers
 import javax.inject._
 import play.api._
 import play.api.mvc._
+import play.Play
 
 /**
  * This controller creates an `Action` to handle HTTP requests to the
  * application's home page.
  */
 @Singleton
-class HomeController @Inject() extends Controller {
+class HomeController @Inject()(environment: play.api.Environment,configuration: play.api.Configuration) 
+extends Controller {
 
   /**
    * Create an Action to render an HTML page.
@@ -26,6 +28,6 @@ class HomeController @Inject() extends Controller {
     Ok("Hello World!")
   }
   def bot = Action { implicit request =>
-    Ok(views.html.bot.render("はじめてのボット", "サンプルチャットアプリ"))
+    Ok(views.html.bot.render("はじめてのボット", configuration.underlying.getString("bot.src")))
   }
 }
